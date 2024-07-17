050693d4654e063bd5ab80d9c377073f68ae6e99
Juntao Qiu <juntao.qiu@gmail.com>
2024-07-17 16:49:54 +1000

    talk to board API through relay + graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import "./App.css";
+import { graphql, useLazyLoadQuery } from "react-relay";
+
+const rootBoardId = "8b03c009-3f38-4e6b-a25e-93aff63d56f1";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const data = useLazyLoadQuery(
+    graphql`
+      query AppBoardQuery($boardId: ID!) {
+        board(id: $boardId) {
+          name
+          columns {
+            id
+            name
+            position
+            cards {
+              id
+              title
+              description
+            }
+          }
+        }
+      }
+    `,
+    { boardId: rootBoardId }
+  );
+
+  if (!data.board) {
+    return <div className={`text-lg`}>something went wrong</div>;
+  }
+
+  return <div>{data.board.name}</div>;
 }
 
 export default App;

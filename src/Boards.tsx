bacae54b49ffd05f95d580b2bd92deede59c6744
Juntao Qiu <juntao.qiu@gmail.com>
2024-07-21 10:34:42 +1000

    clean up a bit

--- src/Boards.tsx
+++ src/Boards.tsx
@@ -1,50 +1,6 @@
 import { graphql, useLazyLoadQuery } from "react-relay";
 import { Link } from "react-router-dom";
 import { Header } from "./components/Header.tsx";
-import {Button, Input} from "@nextui-org/react";
-import { HiOutlinePlus } from "react-icons/hi";
-import { HiOutlineX } from "react-icons/hi";
-
-import { useState } from "react";
-
-const Column = ({ name }: { name: string }) => {
-  const [isEditing, setEditing] = useState<boolean>(false);
-
-  const toggleEditing = () => {
-    setEditing((isEditing) => !isEditing);
-  };
-
-  return (
-    <div className="flex-1 overflow-auto">
-      <div className="bg-gray-100 p-4 rounded-lg flex flex-col max-h-screen">
-        <h2 className="text-lg font-semibold mb-4">{name}</h2>
-        <div className="flex-1 p-4 mb-4 overflow-auto">
-          {" "}
-          {/* Placeholder for tickets */}
-        </div>
-        {isEditing && <div className={`flex flex-col gap-2`}>
-          <Input variant="bordered" type="text" label="Title" placeholder="Type text for the title" autoFocus />
-          <div className={`flex flex-row gap-2 align-middle`}>
-            <Button color="primary" onClick={toggleEditing}>Add</Button>
-            <Button isIconOnly color="default" variant="solid" aria-label="Cancel" onClick={toggleEditing}>
-              <HiOutlineX />
-            </Button>
-          </div>
-        </div>}
-        {!isEditing && (
-          <Button
-            variant="light"
-            color="default"
-            startContent={<HiOutlinePlus />}
-            onClick={toggleEditing}
-          >
-            Add a card
-          </Button>
-        )}
-      </div>
-    </div>
-  );
-};
 
 const Boards = () => {
   const data = useLazyLoadQuery(
@@ -76,12 +32,6 @@ const Boards = () => {
           </li>
         ))}
       </ol>
-
-      <div className="min-h-screen flex overflow-hidden gap-4 py-4">
-        <Column name={"To Do"} />
-        <Column name={"In Progress"} />
-        <Column name={"Done"} />
-      </div>
     </div>
   );
 };

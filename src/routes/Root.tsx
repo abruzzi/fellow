d57f85cdd64a2354f6f0f9cd408536b866bd343d
Juntao Qiu <juntao.qiu@gmail.com>
2024-07-23 21:53:14 +1000

    refine the navigation

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,37 +1,64 @@
 import React from "react";
 
+import { Outlet } from "react-router-dom";
+
 import {
   Navbar,
-  NavbarBrand,
   NavbarContent,
   NavbarItem,
   Link,
-  Button,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  Avatar,
 } from "@nextui-org/react";
-import { Outlet } from "react-router-dom";
 
-export default function Navigation() {
+function Navigation() {
   return (
     <Navbar isBordered>
-      <NavbarBrand>
-        <p className="font-bold text-inherit">Fellow</p>
-      </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="start">
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="/boards">
+          <Link color="foreground" href="/boards" isActive>
             Boards
           </Link>
         </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+        <NavbarItem>
+          <Link color="foreground" href="/recent">
+            Recent
+          </Link>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
+          <Link color="foreground" href="/starred">
+            Favourite
+          </Link>
         </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent as="div" justify="end">
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name="Jason Hughes"
+              size="sm"
+              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">juntao.qiu@gmail.com</p>
+            </DropdownItem>
+            <DropdownItem key="settings">My Settings</DropdownItem>
+            <DropdownItem key="logout" color="danger">
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </NavbarContent>
     </Navbar>
   );

afd3f2a4fa024b54dc5a559f40e4def6098e1c54
Juntao Qiu <juntao.qiu@gmail.com>
2024-07-25 21:48:58 +1000

    client id, secret and redirect url added to system variable

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import relay from 'vite-plugin-relay';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.GOOGLE_CLIENT_ID': JSON.stringify(process.env.GOOGLE_CLIENT_ID),
+    'process.env.GOOGLE_CLIENT_SECRET': JSON.stringify(process.env.GOOGLE_CLIENT_SECRET),
+    'process.env.OAUTH_REDIRECT_URI': JSON.stringify(process.env.OAUTH_REDIRECT_URI)
+  },
   plugins: [react(), relay]
 })

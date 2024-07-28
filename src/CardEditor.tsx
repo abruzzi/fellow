e844d29b4c6f4c4b94a7a7ca60b5029489893f8d
Juntao Qiu <juntao.qiu@gmail.com>
2024-07-28 19:13:29 +1000

    updated card before add comments

--- src/CardEditor.tsx
+++ src/CardEditor.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import {
   Modal,
   ModalContent,
@@ -9,11 +9,16 @@ import {
   Input,
   Textarea,
   Image,
+  PopoverTrigger,
+  Popover,
+  PopoverContent,
+  Spinner,
 } from "@nextui-org/react";
 import { graphql, useMutation } from "react-relay";
-import { HiOutlineMenu, HiOutlineTrash } from "react-icons/hi";
+import { HiOutlineMenu, HiOutlinePlus, HiOutlineTrash } from "react-icons/hi";
 import { HiOutlineMenuAlt2 } from "react-icons/hi";
 import { HiOutlineTicket } from "react-icons/hi";
+import {FaRegEdit} from "react-icons/fa";
 
 export const CardEditor = ({
   cardId,
@@ -37,42 +42,43 @@ export const CardEditor = ({
   const [error, setError] = useState();
   const [hovered, setHovered] = useState<boolean>(false);
 
-  const [selectedFile, setSelectedFile] = useState(null);
+  // const [selectedFile, setSelectedFile] = useState(null);
   const [uploading, setUploading] = useState(false);
+  const [uploadPopupOpen, setUploadPopupOpen] = useState(false);
   const [imageUrl, setImageUrl] = useState(cardImageUrl);
 
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
-      setSelectedFile(event.target.files[0]);
-    }
-  };
+      // setSelectedFile(event.target.files[0]);
+      const selectedFile = event.target.files[0];
 
-  const handleUpload = async () => {
-    if (!selectedFile) return;
+      if (!selectedFile) return;
 
-    const formData = new FormData();
-    formData.append("file", selectedFile);
-    formData.append("upload_preset", import.meta.env.VITE_UPLOAD_PRESET); // Set your upload preset
-    formData.append("cloud_name", import.meta.env.VITE_CLOUDINARY_CLOUD_NAME); // Set your cloud name
+      const formData = new FormData();
+      formData.append("file", selectedFile);
+      formData.append("upload_preset", import.meta.env.VITE_UPLOAD_PRESET); // Set your upload preset
+      formData.append("cloud_name", import.meta.env.VITE_CLOUDINARY_CLOUD_NAME); // Set your cloud name
 
-    setUploading(true);
+      setUploading(true);
+      setUploadPopupOpen(false);
 
-    try {
-      const response = await fetch(
-        `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`,
-        {
-          method: "POST",
-          body: formData,
-        },
-      );
-      const data = await response.json();
-      setImageUrl(data.secure_url);
-    } catch (e: unknown) {
-      console.log(error);
-      // @ts-expect-error
-      setError(e.message);
-    } finally {
-      setUploading(false);
+      try {
+        const response = await fetch(
+          `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`,
+          {
+            method: "POST",
+            body: formData,
+          },
+        );
+        const data = await response.json();
+        setImageUrl(data.secure_url);
+      } catch (e: unknown) {
+        console.log(error);
+        // @ts-expect-error
+        setError(e.message);
+      } finally {
+        setUploading(false);
+      }
     }
   };
 
@@ -110,6 +116,9 @@ export const CardEditor = ({
     });
   };
 
+  const [editingTitle, setEditingTitle] = useState<boolean>(false);
+  const [editingDescription, setEditingDescription] = useState<boolean>(false);
+
   const onTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
   };
@@ -119,8 +128,13 @@ export const CardEditor = ({
   };
 
   return (
-    <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
-      <ModalContent>
+    <Modal
+      isOpen={isOpen}
+      onOpenChange={onOpenChange}
+      placement="top-center"
+      size="2xl"
+    >
+      <ModalContent className="px-10 py-6">
         {(onClose) => (
           <>
             <ModalHeader className="flex flex-col gap-1">
@@ -131,52 +145,126 @@ export const CardEditor = ({
             </ModalHeader>
             {error && <p className="text-red-500">{error}</p>}
             <ModalBody>
-              <Input
-                autoFocus
-                endContent={<HiOutlineMenu />}
-                label="Title"
-                placeholder="Enter card title"
-                value={title}
-                onChange={onTitleChange}
-                radius="sm"
-              />
-              <Textarea
-                label="Description"
-                endContent={<HiOutlineMenuAlt2 />}
-                placeholder="Enter your description"
-                value={description}
-                onChange={onDescriptionChange}
-                radius="sm"
-              />
-              <Input type="file" onChange={handleFileChange} />
-              <Button onPress={handleUpload} disabled={uploading}>
-                {uploading ? "Uploading..." : "Upload"}
-              </Button>
-
-              {imageUrl && (
-                <div
-                  className="relative"
-                  onMouseEnter={() => setHovered(true)}
-                  onMouseLeave={() => setHovered(false)}
-                >
-                  {hovered && (
-                    <div className="absolute right-2 top-2 delay-100 z-20">
-                      <Button
-                        size="sm"
-                        isIconOnly
-                        color="danger"
-                        variant="light"
-                        aria-label="Delete"
-                        onPress={() => setImageUrl(undefined)}
-                      >
-                        <HiOutlineTrash />
-                      </Button>
-                    </div>
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col">
+                  <h4 className="font-bold text-slate-600">Title</h4>
+                  {editingTitle ? (
+                    <Input
+                      autoFocus
+                      variant="flat"
+                      endContent={<HiOutlineMenu />}
+                      label="Title"
+                      placeholder="Enter card title"
+                      value={title}
+                      onChange={onTitleChange}
+                      radius="none"
+                    />
+                  ) : (
+                    <p onClick={() => setEditingTitle(true)}>{title}</p>
                   )}
-                  <Image src={imageUrl} alt="Uploaded file" />
                 </div>
-              )}
+
+                <div className="flex flex-col">
+                  <div className="flex flex-row justify-between items-center">
+                    <h4 className="font-bold text-slate-600">Description</h4>
+                    <Button isIconOnly variant="light" onPress={() => setEditingDescription(true)}>
+                      <FaRegEdit />
+                    </Button>
+                  </div>
+                  {editingDescription ? (
+                    <Textarea
+                      label="Description"
+                      endContent={<HiOutlineMenuAlt2 />}
+                      placeholder="Enter your description"
+                      value={description}
+                      onChange={onDescriptionChange}
+                      radius="none"
+                    />
+                  ) : (
+                    <p onClick={() => setEditingDescription(true)}>
+                      {description}
+                    </p>
+                  )}
+                </div>
+
+                <div className="flex flex-col">
+                  <div className="flex flex-row justify-between items-center">
+                    <h4 className="font-bold text-slate-600">Image</h4>
+                    <Popover
+                      placement="bottom"
+                      showArrow
+                      offset={10}
+                      isOpen={uploadPopupOpen}
+                      onOpenChange={(open) => setUploadPopupOpen(open)}
+                    >
+                      <PopoverTrigger>
+                        <Button
+                          isIconOnly
+                          variant="light"
+                          onPress={() => setUploadPopupOpen(true)}
+                        >
+                          <HiOutlinePlus />
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-[240px]">
+                        {(titleProps) => (
+                          <div className="px-1 py-2 w-full">
+                            <p
+                              className="text-small font-bold text-foreground"
+                              {...titleProps}
+                            >
+                              Upload
+                            </p>
+                            <div className="mt-2 flex flex-col gap-2 w-full">
+                              <Input
+                                className="flex-1"
+                                type="file"
+                                onChange={handleFileChange}
+                              />
+                            </div>
+                          </div>
+                        )}
+                      </PopoverContent>
+                    </Popover>
+                  </div>
+                  <div className="flex flex-row">
+                    <div className="max-w-36">
+                      {imageUrl && (
+                        <div
+                          className="relative"
+                          onMouseEnter={() => setHovered(true)}
+                          onMouseLeave={() => setHovered(false)}
+                        >
+                          {uploading && (
+                            <div className="absolute right-2 top-2 delay-100 z-20">
+                              <Spinner color="default" />
+                            </div>
+                          )}
+                          {hovered && (
+                            <div className="absolute right-2 top-2 delay-100 z-20">
+                              <Button
+                                size="sm"
+                                isIconOnly
+                                color="danger"
+                                variant="light"
+                                aria-label="Delete"
+                                onPress={() => setImageUrl(undefined)}
+                              >
+                                <HiOutlineTrash />
+                              </Button>
+                            </div>
+                          )}
+                          <Image src={imageUrl} alt="Uploaded file" />
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <hr />
             </ModalBody>
+
             <ModalFooter>
               <Button color="default" variant="flat" onPress={onClose}>
                 Cancel
--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,13 +1,21 @@
 import {
+  Avatar,
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
   Link,
   Navbar,
   NavbarContent,
   NavbarItem,
 } from "@nextui-org/react";
-import React from "react";
+import React, { useState } from "react";
 import { graphql, usePreloadedQuery } from "react-relay";
 import { CurrentUserQuery as CurrentUserQueryType } from "./queries/__generated__/CurrentUserQuery.graphql.ts";
 import { UserMenu } from "./UserMenu.tsx";
+import { useLocation } from "react-router-dom";
+import { MdOutlineExpandLess, MdOutlineExpandMore } from "react-icons/md";
 
 // eslint-disable-next-line react/prop-types
 export function Navigation({ queryRef }) {
@@ -22,23 +30,89 @@ export function Navigation({ queryRef }) {
     queryRef,
   );
 
+  const location = useLocation();
+
+  const [recentListOpen, setRecentListOpen] = useState<boolean>(false);
+  const [favouriteListOpen, setFavouriteListOpen] = useState<boolean>(false);
+  const handleRecentListOpen = (isOpen: boolean) => {
+    setRecentListOpen(isOpen);
+  };
+  const handleFavouriteListOpen = (isOpen: boolean) => {
+    setFavouriteListOpen(isOpen);
+  };
+
   return (
     <Navbar isBordered>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
           <Link color="foreground" href="/boards">
-            Boards
+            <Button
+              variant="light"
+              className={`${location.pathname.includes("/boards") ? "font-bold" : ""} text-lg`}
+            >
+              Boards
+            </Button>
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="/recent">
-            Recent
-          </Link>
+          <Dropdown placement="bottom" onOpenChange={handleRecentListOpen}>
+            <DropdownTrigger>
+              <Button
+                variant="light"
+                endContent={
+                  recentListOpen ? (
+                    <MdOutlineExpandLess />
+                  ) : (
+                    <MdOutlineExpandMore />
+                  )
+                }
+                className="text-lg"
+              >
+                Recent
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat">
+              <DropdownItem key="profile" href="/profile">
+                Board 1
+              </DropdownItem>
+              <DropdownItem key="settings" href="/settings">
+                Board 2
+              </DropdownItem>
+              <DropdownItem key="logout" href="/logout">
+                Board 3
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="/starred">
-            Favourite
-          </Link>
+          <Dropdown placement="bottom" onOpenChange={handleFavouriteListOpen}>
+            <DropdownTrigger>
+              <Button
+                variant="light"
+                endContent={
+                  recentListOpen ? (
+                    <MdOutlineExpandLess />
+                  ) : (
+                    <MdOutlineExpandMore />
+                  )
+                }
+                className="text-lg"
+              >
+                Favourite
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat">
+              <DropdownItem key="profile" href="/profile">
+                Board 1
+              </DropdownItem>
+              <DropdownItem key="settings" href="/settings">
+                Board 2
+              </DropdownItem>
+              <DropdownItem key="logout" href="/logout">
+                Board 3
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </NavbarItem>
       </NavbarContent>
 

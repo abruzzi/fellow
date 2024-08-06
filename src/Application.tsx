1903546d34195bb280a8fffa1246a1e69cf6cc23
Juntao Qiu <juntao.qiu@gmail.com>
2024-08-06 20:02:10 +1000

    avoid infinite loading when unlogined

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+
 import { Root } from "./routes/Root.tsx";
 import { ProtectedRoute } from "./routes/ProtectedRoute.tsx";
 import { Boards } from "./Boards.tsx";
@@ -6,42 +8,12 @@ import { AcceptInvitation } from "./routes/AcceptInvitation.tsx";
 import { Login } from "./routes/Login.tsx";
 import { Logout } from "./routes/Logout.tsx";
 import { OAuthCallback } from "./OAuthCallback.tsx";
-import React, { useCallback, useEffect } from "react";
-import { useQueryLoader } from "react-relay";
-import { ApplicationQuery as ApplicationQueryType } from "./queries/__generated__/ApplicationQuery.graphql.ts";
-import { ApplicationQuery } from "./queries/ApplicationQuery.ts";
 import { BoardScreen } from "./BoardScreen.tsx";
-import { Loading } from "./Loading.tsx";
 
 export const Application = () => {
-  const [applicationQueryRef, loadApplicationQuery] =
-    useQueryLoader<ApplicationQueryType>(ApplicationQuery);
-
-  const refreshApplicationQuery = useCallback(() => {
-    loadApplicationQuery({}, { fetchPolicy: "store-and-network" });
-  }, [loadApplicationQuery]);
-
-  useEffect(() => {
-    if (!applicationQueryRef) {
-      refreshApplicationQuery();
-    }
-  }, [refreshApplicationQuery, applicationQueryRef]);
-
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          applicationQueryRef ? (
-            <Root
-              queryRef={applicationQueryRef}
-              refreshQuery={refreshApplicationQuery}
-            />
-          ) : (
-            <Loading />
-          )
-        }
-      >
+      <Route path="/" element={<Root />}>
         <Route index element={<Navigate to="/boards" replace />} />
         <Route
           path="boards"
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 
 import { Outlet } from "react-router-dom";
 import { Navigation } from "../Navigation.tsx";
 import { ErrorBoundary } from "react-error-boundary";
 import { FavoriteBoardProvider } from "../FavoriteBoardContext.tsx";
 import { UserProvider } from "../UserContext.tsx";
+import { useQueryLoader } from "react-relay";
+import { ApplicationQuery as ApplicationQueryType } from "../queries/__generated__/ApplicationQuery.graphql.ts";
+import { ApplicationQuery } from "../queries/ApplicationQuery.ts";
+import { Loading } from "../Loading.tsx";
 
 // eslint-disable-next-line react/prop-types
-const Root = ({ queryRef, refreshQuery }) => {
+const Root = () => {
+  const [applicationQueryRef, loadApplicationQuery] =
+    useQueryLoader<ApplicationQueryType>(ApplicationQuery);
+
+  const refreshApplicationQuery = useCallback(() => {
+    loadApplicationQuery({}, { fetchPolicy: "store-and-network" });
+  }, [loadApplicationQuery]);
+
+  useEffect(() => {
+    if (!applicationQueryRef) {
+      refreshApplicationQuery();
+    }
+  }, [refreshApplicationQuery, applicationQueryRef]);
+
+  if (!applicationQueryRef) {
+    return <Loading />;
+  }
+
   return (
     <div className="h-screen flex flex-col max-h-screen">
       <ErrorBoundary
         fallback={<div>Something went wrong on the root level</div>}
       >
-        <FavoriteBoardProvider queryRef={queryRef} refresh={refreshQuery}>
-          <UserProvider queryRef={queryRef}>
+        <FavoriteBoardProvider
+          queryRef={applicationQueryRef}
+          refresh={refreshApplicationQuery}
+        >
+          <UserProvider queryRef={applicationQueryRef}>
             <Navigation />
           </UserProvider>
 
-          <Outlet context={{ queryRef, refreshQuery }} />
+          <Outlet
+            context={{
+              queryRef: applicationQueryRef,
+              refreshQuery: refreshApplicationQuery,
+            }}
+          />
         </FavoriteBoardProvider>
       </ErrorBoundary>
     </div>
